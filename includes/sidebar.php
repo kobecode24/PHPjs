5ed09b55dffb9df1c2b671cd538d6e0a2cb10cd6
kobe <youcode@skiff.com>
2023-12-07 20:48:25 +0100

    added a sidebar to the includes

---
+++ includes/sidebar.php
@@ -0,0 +1,17 @@
+<div class="sidebar col-auto col-md-3 min-vh-100 ">
+    <div class="side-content d-flex flex-column align-items-center">
+        <!-- Profile Section -->
+        <div class="profil d-flex flex-column align-items-center gap-3">
+            <img src="../public/assets/img-dash/user.png" alt="dashboard-profile">
+            <span class="d-none d-md-inline">user name</span>
+        </div>
+        <!-- Navigation Links -->
+        <ul class="nav d-flex flex-column">
+            <li><a href="#" class="text-decoration-none text-white px-4 py-2"><i class="fa-solid fa-border-all"></i><span class="d-none d-md-inline">Dashboard</span></a></li>
+            <li><a href="#" class="text-decoration-none text-white px-4 py-2"><i class="fa-solid fa-users"></i><span class="d-none d-md-inline">Cast</span></a></li>
+            <li><a href="#" class="text-decoration-none text-white px-4 py-2"><i class="fa-solid fa-film"></i><span class="d-none d-md-inline">Film</span></a></li>
+            <li><a href="#" class="text-decoration-none text-white px-4 py-2"><i class="fa-solid fa-theater-masks"></i><span class="d-none d-md-inline">Genre</span></a></li>
+            <li><a href="#" class="text-decoration-none text-white px-4 py-2"><i class="fa-solid fa-arrow-right-from-bracket"></i><span class="d-none d-md-inline">Log out</span></a></li>
+        </ul>
+    </div>
+</div>

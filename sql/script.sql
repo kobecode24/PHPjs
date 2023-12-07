dcb269dc9c36f105314a85f459f8048b7e06a59b
kobe <youcode@skiff.com>
2023-12-07 21:02:24 +0100

    DB sql script

--- sql/script.sql
+++ sql/script.sql
@@ -1,49 +1,58 @@
-/*CREATE DATABASE MovieMingle;
+-- Create the MovieMingle Database
+CREATE DATABASE IF NOT EXISTS MovieMingle;
 USE MovieMingle;
-*/
 
-CREATE TABLE genre (
-                       id INT AUTO_INCREMENT PRIMARY KEY,
-                       name VARCHAR(255) NOT NULL
+-- Create the 'genre' table
+CREATE TABLE IF NOT EXISTS genre (
+                                     id INT AUTO_INCREMENT PRIMARY KEY,
+                                     name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE status (
-                        id INT AUTO_INCREMENT PRIMARY KEY,
-                        status VARCHAR(255) NOT NULL
+-- Create the 'user' table
+CREATE TABLE IF NOT EXISTS user (
+                                    id INT AUTO_INCREMENT PRIMARY KEY,
+                                    username VARCHAR(255) NOT NULL UNIQUE,
+                                    password VARCHAR(255) NOT NULL,
+                                    isadmin BOOLEAN NOT NULL
 );
 
-CREATE TABLE user (
-                      id INT AUTO_INCREMENT PRIMARY KEY,
-                      username VARCHAR(255) NOT NULL UNIQUE,
-                      password VARCHAR(255) NOT NULL,
-                      isadmin BOOLEAN NOT NULL
+-- Create the 'cast' table
+CREATE TABLE IF NOT EXISTS `cast` (
+                                      id INT AUTO_INCREMENT PRIMARY KEY,
+                                      firstname VARCHAR(255) NOT NULL,
+                                      lastname VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE `cast` (
-                        id INT AUTO_INCREMENT PRIMARY KEY,
-                        firstname VARCHAR(255) NOT NULL,
-                        lastname VARCHAR(255) NOT NULL
+-- Create the 'film' table
+CREATE TABLE IF NOT EXISTS film (
+                                    id INT AUTO_INCREMENT PRIMARY KEY,
+                                    title VARCHAR(255) NOT NULL,
+                                    duration INT NOT NULL,
+                                    releasedate DATETIME NOT NULL,
+                                    genre_id INT,
+                                    FOREIGN KEY (genre_id) REFERENCES genre(id)
 );
 
-CREATE TABLE film (
-                      id INT AUTO_INCREMENT PRIMARY KEY,
-                      title VARCHAR(255) NOT NULL,
-                      duration INT NOT NULL,
-                      releasedate DATETIME NOT NULL,
-                      genre_id INT,
-                      FOREIGN KEY (genre_id) REFERENCES genre(id)
+-- Create the 'film_cast' table
+CREATE TABLE IF NOT EXISTS film_cast (
+                                         id INT AUTO_INCREMENT PRIMARY KEY,
+                                         film_id INT NOT NULL,
+                                         cast_id INT NOT NULL,
+                                         role VARCHAR(255) NOT NULL,
+                                         FOREIGN KEY (film_id) REFERENCES film(id) ON DELETE CASCADE ON UPDATE CASCADE,
+                                         FOREIGN KEY (cast_id) REFERENCES `cast`(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE film_cast (
-                           id INT AUTO_INCREMENT PRIMARY KEY,
-                           film_id INT NOT NULL,
-                           cast_id INT NOT NULL,
-                           role VARCHAR(255) NOT NULL,
-                           FOREIGN KEY (film_id) REFERENCES film(id),
-                           FOREIGN KEY (cast_id) REFERENCES `cast`(id)
+-- Create the 'status' table with foreign keys to 'user' and 'film'
+CREATE TABLE IF NOT EXISTS status (
+                                      id INT AUTO_INCREMENT PRIMARY KEY,
+                                      status VARCHAR(255) NOT NULL,
+                                      user_id INT,
+                                      film_id INT,
+                                      FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE,
+                                      FOREIGN KEY (film_id) REFERENCES film(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
--- Inserting genres
 INSERT INTO genre (name) VALUES
                              ('Action'),
                              ('Comedy'),
@@ -56,23 +65,19 @@ INSERT INTO genre (name) VALUES
                              ('Documentary'),
                              ('Crime');
 
--- Inserting statuses
-INSERT INTO status (status) VALUES
-                                ('Favorite'),
-                                ('WatchLater'),
-                                ('Both');
+
 -- Inserting users
 INSERT INTO user (username, password, isadmin) VALUES
-                                                   ('alice', 'hashed_password1', FALSE),
-                                                   ('bob', 'hashed_password2', TRUE),
-                                                   ('charlie', 'hashed_password3', FALSE),
-                                                   ('david', 'hashed_password4', FALSE),
-                                                   ('eva', 'hashed_password5', TRUE),
-                                                   ('frank', 'hashed_password6', FALSE),
-                                                   ('grace', 'hashed_password7', FALSE),
-                                                   ('henry', 'hashed_password8', TRUE),
-                                                   ('irene', 'hashed_password9', FALSE),
-                                                   ('jack', 'hashed_password10', FALSE);
+                                                   ('alice', '$2y$10$jyJoMBkjajXe7Qgw1wBqs.Bz9DpBfL3FLsM5oBeYEfPtTW/dsqJk.', FALSE),
+                                                   ('bob', '$2y$10$J82VJQv.K8fQrhKpA3JXd.UTbQvP2hUkMu2up3TwLTOrz1f4zw4d.', TRUE),
+                                                   ('charlie', '$2y$10$2lP.C0H/c4S7P9ABnUQrse0hUYabHOIZTxx6R/SWVL9Khw7ywUcGy', FALSE),
+                                                   ('david', '$2y$10$XEDFFcWu2C.Q4DCj.pshHORAAVIRtY6P42BEnGLxN/4kYcpG3YbmO', FALSE),
+                                                   ('eva', '$2y$10$Sf04XxVmhMaCUFRpXvMmw.WehjhGixP0k/I1zbvkAfR41HPSqouTK', TRUE),
+                                                   ('frank', '$2y$10$4imTO/xjjxn0Y8zhntG7a.jFaRNj.n/AicNRVjKGn8/IZPeTNYh22', FALSE),
+                                                   ('grace', '$2y$10$R6z6ieOPZCKiZlCmsz68KeWOSVjW0.M7pfVN80vRpJUUL/IdngQpm', FALSE),
+                                                   ('henry', '$2y$10$H2MG.TksEVZZhGxj16PHLut9mksqtgnzCaTAX.QiI2zcNT7LxaFhi', TRUE),
+                                                   ('irene', '$2y$10$duh48koktfZ/JjbSuRRT..OqIXsX6Ll9EY5oAiayxoTn3N6s0u7t.', FALSE),
+                                                   ('jack', '$2y$10$kGINS5U95jcYFOVXnCJTBOVOk.eVzaGA2tMu5ilPhUfH6Z3ENGo22', FALSE);
 
 -- Inserting cast members
 INSERT INTO cast (firstname, lastname) VALUES
@@ -118,5 +123,13 @@ INSERT INTO film_cast (film_id, cast_id, role) VALUES
                                                    (3, 5, 'Bane'),
                                                    (7, 5, 'John Fitzgerald'),
                                                    (8, 6, 'Nick Dunne');
+
+
+INSERT INTO status (status, user_id, film_id) VALUES
+                                                  ('Favourites', 1, 5),
+                                                  ('Favourites', 2, 3),
+                                                  ('To Watch Later', 3, 2),
+                                                  ('To Watch Later', 4, 7);
+
 
 

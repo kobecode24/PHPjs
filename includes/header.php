53aa22f8e7be99b8909b03be0d6e4df09c24b151
kobe <youcode@skiff.com>
2023-12-07 20:52:02 +0100

    fix the header include

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,14 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous" defer></script>
     <script src="../public/js/main.js" defer></script>
     <link rel="stylesheet" href="<?php echo $page_css ?? '../public/css/sign-up.css'; ?>">
+    <?php
+    if (isset($dashcss)) {
+        echo "<link rel=\"stylesheet\" href=\"$dashcss\">";
+    }
+    ?>
+
 </head>
+
 <body>
 
 <nav>
@@ -17,18 +24,18 @@
         <div class="d-flex w-sm-100 align-items-center justify-content-between gap-3">
             <div class="logo"><img class="img-fluid" src="../public/assets/imgs/logo.png" alt="logo"></div>
             <?php if (!isset($hideBurgerMenu)): ?>
-                <div class="menu"><i class="fa-solid fa-bars burger-menu fs-3 text-white"></i></div>
+            <div class="menu"><i class="fa-solid fa-bars burger-menu fs-3 text-white"></i></div>
         </div>
-            <div class="sub-menu d-flex w-25 justify-content-center">
-                <ul class="">
-                    <li><a class="text-capitalize" href="./contact.html">contact</a></li>
-                    <li><a class="text-capitalize" href="./dashboard.html">dashboard</a></li>
-                    <li><a class="text-capitalize" href="./about.html">about</a></li>
-                </ul>
-            </div>
-            <div class="search-wrapper flex-grow-1">
-                <input class="py-2 px-3 rounded-2 w-100 border-0 d-none d-md-block" type="text" placeholder="Search">
-            </div>
+        <div class="sub-menu d-flex w-25 justify-content-center">
+            <ul class="">
+                <li><a class="text-capitalize" href="./contact.html">contact</a></li>
+                <li><a class="text-capitalize" href="./dashboard.html">dashboard</a></li>
+                <li><a class="text-capitalize" href="./about.html">about</a></li>
+            </ul>
+        </div>
+        <div class="search-wrapper flex-grow-1">
+            <input class="py-2 px-3 rounded-2 w-100 border-0 d-none d-md-block" type="text" placeholder="Search">
+        </div>
 
         <div class="sign-in-wrapper d-flex align-items-center gap-3">
             <a href="./MoviesSeries.html" class="fw-semibold text-white d-none d-md-block"> Watchlist</a>

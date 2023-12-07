9c5eda99e3f77602f45c6210848246ad0d7ec7a9
kobe <youcode@skiff.com>
2023-12-07 21:00:52 +0100

    added openModal js

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,16 @@ const burgerMenu  = document.querySelector('.burger-menu ');
 burgerMenu.addEventListener('click', () => {
     menu.classList.toggle("active");
 })
+function openEditModal(film) {
+    document.getElementById('editFilmId').value = film.id;
+    document.getElementById('editFilmTitle').value = film.title;
+    document.getElementById('editFilmDuration').value = film.duration;
+    document.getElementById('editFilmReleaseDate').value = film.releasedate;
+
+    const genreSelect = document.getElementById('editFilmGenre');
+    genreSelect.value = film.genre_id;
+
+    let editModal = new bootstrap.Modal(document.getElementById('editFilmModal'));
+    editModal.show();
+}
+
